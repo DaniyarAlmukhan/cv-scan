4c5b9e4512a8e452b3e6cb489214f937c9160b9c
Daniyar Al'mukhan <daniyar.almukhan@nu.edu.kz>
2025-01-27 12:29:06 +0500

    fix: react-scripts

--- src/requests/onboarding.request.ts
+++ src/requests/onboarding.request.ts
@@ -3,5 +3,5 @@ import api from './api';
 const baseUrl = "http://91.185.21.250/api";
 
 export const getChatResponse = (question: string) => {
-  return api.post(`/api/onboarding/query?question=${question}`).then(res => res.data);
+  return api.post(`/onboarding/query?question=${question}`).then(res => res.data);
 } 
